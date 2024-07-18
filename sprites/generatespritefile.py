2c0e56c5e7020716eae84061a7cbed5230564f18
Valmir Filho <119276059+ValmirNogFilho@users.noreply.github.com>
2024-07-17 21:14:37 -0300

    Update and rename desenhando.py to generatespritefile.py

--- sprites/generatespritefile.py
+++ sprites/generatespritefile.py
@@ -1,19 +1,14 @@
 from PIL import Image
 
-# Abra uma imagem
 image = Image.open('fim.png')
 
-# Converta a imagem para o modo RGB (se não estiver nesse modo)
 image = image.convert('RGBA')
 
-# Obtenha as dimensões da imagem
 width, height = image.size
 
 with open("fim.back", "a") as f:
-    # Acesse e manipule cada pixel
     for x in range(width):
         for y in range(height):
-            # Obtenha o valor do pixel (R, G, B)
             r, g, b, a = image.getpixel((x, y))
             if(a == 0): 
                 r, g, b = 192, 224, 224
